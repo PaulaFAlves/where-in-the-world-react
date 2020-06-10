53370fa4c383194cfdc54691f419e42cbfa42f53
Paula Alves <paulafernandaalves@icloud.com>
2020-06-10 12:20:02 -0300

    Updates

---
+++ precache-manifest.be4beead0a867834ca42aecf761284df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e43d6857fe54b1228ea93d1a912a5dd",
+    "url": "/where-in-the-world-react/index.html"
+  },
+  {
+    "revision": "d492edc30db08f98c798",
+    "url": "/where-in-the-world-react/static/css/main.54827860.chunk.css"
+  },
+  {
+    "revision": "d24ae140cf029abe4088",
+    "url": "/where-in-the-world-react/static/js/2.df27581c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/where-in-the-world-react/static/js/2.df27581c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d492edc30db08f98c798",
+    "url": "/where-in-the-world-react/static/js/main.b13ebf7f.chunk.js"
+  },
+  {
+    "revision": "bbac1b28d77b78eed355",
+    "url": "/where-in-the-world-react/static/js/runtime-main.86ec21e7.js"
+  }
+]);
