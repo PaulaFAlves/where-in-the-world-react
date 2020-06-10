67a4adb54c91eb1dd0b725e1e8e584861a0b2b07
Paula Alves <paulafernandaalves@icloud.com>
2020-06-10 16:56:06 -0300

    fixed bugs

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Details from './pages/Details';
 
 const Routes = () => {
 	return (
-		<HashRouter basename={process.env.PUBLIC_ULR}>
+		<BrowserRouter basename={process.env.PUBLIC_ULR + /}>
 			<Route component={Home} path="/" exact />
 			<Route component={Details} path="/details"/>
-		</HashRouter>
+		</BrowserRouter>
 	)
 }
 
